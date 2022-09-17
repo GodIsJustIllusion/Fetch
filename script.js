32f6caa0e19baaa70663c2c667505b8cf014b9d9
Igor_A <87186964+GodIsJustIllusion@users.noreply.github.com>
2022-09-17 23:06:28 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
-let body = document.querySelector('body');
-let ul = document.createElement('ul');
-let img = document.createElement('img');
-body.append(img);
-body.append(ul);
+let content = document.querySelector('#content');
 
-let createrUser = fetch ('https://randomuser.me/api/');
-createrUser 
-    .then (Response => {
-        if (Response.ok) {
-            Response.json().then(user => {
-                console.log(user);
-
-                getUser(user);
-
-                img.setAttribute('src', user.results[0].picture.large);
-
-                function getUser(obj) {
-                    getUserData(obj);
-
-                    function getUserData(object) {
-
-                        for (let key in object) {
-
-                            if (typeof object[key] === 'object') {
-                                getUserData(object[key]);
-                                
-                            } else {
-                                let li = document.createElement('li');
-                                ul.append(li);
-                                li.innerText = key + ": " + object[key];
+async function creatingUser() {
+    let div = document.createElement('div');
+    content.append(div);
+    let ul = document.createElement('ul');
+    let img = document.createElement('img');
+    div.append(img);
+    div.append(ul);
+    await fetch ('https://randomuser.me/api/')
+        .then (Response => {
+            if (Response.ok) {
+                Response.json()
+                .then(user => {
+                    console.log(user);
+    
+                    getUser(user);
+    
+                    img.setAttribute('src', user.results[0].picture.large);
+    
+                    function getUser(obj) {
+                        getUserData(obj);
+    
+                        function getUserData(object) {
+    
+                            for (let key in object) {
+    
+                                if (typeof object[key] === 'object') {
+                                    getUserData(object[key]);
+                                    
+                                } else {
+                                    let li = document.createElement('li');
+                                    ul.append(li);
+                                    li.innerText = key + ": " + object[key];
+                                }
                             }
                         }
                     }
-                }
-            })
-            
-        }
-    })
-    .catch(error => {
-        console.log(error);
-    });
+                })
+                
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+creatingUser();
+creatingUser();
+creatingUser();
